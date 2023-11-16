c124e509539ea8aabe0cf20b0c42e6de49071580
ahmedqadar <abdulnet029@gmail.com>
2023-11-16 21:12:20 +0300

    Routes

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+  {/*{code here}*/}
+  <h1>Directors Page</h1>
+  {directors.map((director) => (
+    <div key={director.name}>
+      {/* director's name */}
+      <p>{director.name}</p>
+  
+      {/* list of movies */}
+      <ul>
+        {director.movies.map((movie, index) => (
+          <li key={index}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))}
+  </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+  {/*{code here}*/}
+  <h1>Movies Page</h1>
+  {movies.map((movie) => (
+    <div key={movie.title}>
+      {/* movie's title */}
+      <h2>{movie.title}</h2>
+  
+      {/* movie's time */}
+      <p>Time: {movie.time}</p>
+  
+      {/* list of genres */}
+      <ul>
+        {movie.genres.map((genre, index) => (
+          <li key={index}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))}
+  </div>;
 }
 
 export default Movies;
